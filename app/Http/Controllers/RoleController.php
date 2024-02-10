fa31cab905c69e653d75ba9933678ef3bf17ae5e
Zubayer Hossain <zubayer94@gmail.com>
2024-02-10 22:48:25 +0600

    defined role middleware

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -9,6 +9,10 @@ use App\Http\Requests\RoleRequest;
 
 class RoleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      */
